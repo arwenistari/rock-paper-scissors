51e2a604a66ef3808746f903d8b1c193573f8503
ArwenIstari <147433327+arwenistari@users.noreply.github.com>
2024-04-24 11:49:05 -0500

    Create function for game round that indicates who wins based off player prompt selection

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 function getComputerChoice() {
     let choice = ['Rock', 'Paper', 'Scissors'];
-    return choice(Math.floor(Math.random() * choice.length));
+    return choice[Math.floor(Math.random() * choice.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return(`It's a tie! You both picked ${playerSelection}!`);
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return("You Win! Rock beats Scissors");
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return("You Win! Paper beats Rock");
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return("You Win! Scissors beat Paper");
+    } else {
+        return(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    }
+}
+
+function getHumanChoice() {
+    const playerSelection = prompt("Start the game by picking 'Rock', 'Paper', or 'Scissors'.");
+}
+
+
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
