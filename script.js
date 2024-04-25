c21643c2d81ea96d45703f21a980e71fc9aead94
ArwenIstari <147433327+arwenistari@users.noreply.github.com>
2024-04-25 13:08:54 -0500

    Fix and better organize functions to be easily readable

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-function getComputerChoice() {
+function computerChoice() {
     let choice = ['Rock', 'Paper', 'Scissors'];
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return(`It's a tie! You both picked ${playerSelection}!`);
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+function playerChoice() {
+    return prompt("Start the game by picking 'Rock', 'Paper', or 'Scissors'.");
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return(`It's a tie! You both picked ${playerChoice}!`);
+    } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
         return("You Win! Rock beats Scissors");
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
         return("You Win! Paper beats Rock");
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
         return("You Win! Scissors beat Paper");
     } else {
-        return(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        return(`You Lose! ${computerChoice} beats ${playerChoice}`);
     }
 }
 
 
-const playerSelection = prompt("Start the game by picking 'Rock', 'Paper', or 'Scissors'.");
+const playerSelection = playerChoice();
 console.log(playerSelection);
-const computerSelection = getComputerChoice();
+const computerSelection = computerChoice();
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
+
+
+
+
